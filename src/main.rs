8bf8f9037bc7a0daacc3293923ea876cf39637dc
Eddie1O <andrzej.witowski@itinberlin.de>
2024-09-18 21:33:09 +0200

    fix adc readings by probing multiple times, added HW voltage divider

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use esp_idf_hal::{
     adc::{attenuation, config::Config, AdcChannelDriver, AdcDriver, ADC1},
-    delay::{Delay, FreeRtos},
+    delay::{Delay, Ets, FreeRtos},
     gpio::{Gpio10, Gpio3, Gpio7, Output, PinDriver},
     timer::{self, TimerDriver},
 };
@@ -24,24 +24,27 @@ use esp_idf_svc::hal::prelude::Peripherals;
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     let peripherals = Peripherals::take()?;
-    let delay: Delay = Default::default();
+    let mut led = PinDriver::output(peripherals.pins.gpio8)?;
+    led.set_high()?;
     let mut timer = TimerDriver::new(peripherals.timer00, &timer::config::Config::default())?;
     timer.enable(true)?;
+    let delay: Delay = Default::default();
 
     let mut adc = AdcDriver::new(peripherals.adc1, &Config::new().calibration(true))?;
-    let mut photosensor: esp_idf_hal::adc::AdcChannelDriver<{ attenuation::DB_11 }, _> =
+    let mut photosensor: esp_idf_hal::adc::AdcChannelDriver<0, _> =
         AdcChannelDriver::new(peripherals.pins.gpio3)?;
 
     let mut led = PinDriver::output(peripherals.pins.gpio7)?;
     led.set_low()?;
 
     let mut motor = PinDriver::output(peripherals.pins.gpio10)?;
-    motor.set_low()?;
 
     loop {
         //one loop is exactly 24 hours
         let counter_now = timer.counter().unwrap();
         let counter_after_24_hours = counter_now + 1000000 * 60 * 60 * 24;
+        delay.delay_ms(2000);
+        rotate(&mut motor, &mut adc, &mut photosensor, &mut led);
         delay.delay_ms(1000 * 60 * 60 * 4);
         rotate(&mut motor, &mut adc, &mut photosensor, &mut led);
         delay.delay_ms(1000 * 60 * 60 * 4);
@@ -58,15 +61,25 @@ fn main() -> anyhow::Result<()> {
 fn rotate(
     motor: &mut PinDriver<Gpio10, Output>,
     adc: &mut AdcDriver<ADC1>,
-    photosensor: &mut AdcChannelDriver<{ attenuation::DB_11 }, Gpio3>,
+    photosensor: &mut AdcChannelDriver<0, Gpio3>,
     led: &mut PinDriver<Gpio7, Output>,
 ) {
     led.set_high().unwrap();
     motor.set_high().unwrap();
-    let high_adc_value = 2918;
     FreeRtos::delay_ms(2000);
-    while adc.read(photosensor).unwrap() < high_adc_value {}
-    FreeRtos::delay_ms(170);
-    motor.set_low().unwrap();
-    led.set_low().unwrap();
+    //
+    loop {
+        let mut count = 0;
+        for _ in 0..250 {
+            count += adc.read_raw(photosensor).unwrap();
+
+            Ets::delay_us(10);
+        }
+        if count >= 5800 {
+            FreeRtos::delay_ms(150);
+            motor.set_low().unwrap();
+            led.set_low().unwrap();
+            break;
+        }
+    }
 }
