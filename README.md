1072001446ec367e5e9b6a8e2de2b012b9c822d7
Eddie1O <andrzej.witowski@itinberlin.de>
2026-09-06 20:02:48 +0200

    update packages and code

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ## But why?
 
-My `Cat Mate C300 Automatic 3 Meal Feeder` eletronics died after few months of use. Instead of creating more waste I tried to use one of ESP32-C3's laying around to restore the functionality of this automatic feeder. I have use some spare parts and voilà! It works again and my cat is very happy about it.
+My `Cat Mate C300 Automatic 3 Meal Feeder` electronics died after few months of use. Instead of creating more waste I tried to use one of ESP32-C3's laying around to restore the functionality of this automatic feeder. I have used some spare parts and voilà! It works again and my cat is very happy about it.
+
+Firmware is `no_std` [esp-hal](https://github.com/esp-rs/esp-hal) on RISC-V (`riscv32imc-unknown-none-elf`). Nightly is required only for Cargo `build-std`.
+
+## Hardware:
+
+1. Microcontroller: `ESP32C3`
+2. Motor: simple 5V motor
+3. Optoresistor for detecting in-between of bowl sections
+4. Red LED to shine on optoresistor (covered by bowl, when not in-between of 3 bowl spaces)
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,89 @@
-use esp_idf_hal::{
-    adc::{attenuation, config::Config, AdcChannelDriver, AdcDriver, ADC1},
-    delay::{Delay, Ets, FreeRtos},
-    gpio::{Gpio10, Gpio3, Gpio7, Output, PinDriver},
-    timer::{self, TimerDriver},
-};
-use esp_idf_svc::hal::prelude::Peripherals;
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
 
-// ####### GPIO PIN CONFIGURATION ########
-// GPIO 0
-// GPIO 1
-// GPIO 2
-// GPIO 3 - adc photosensor
-// GPIO 4
-// GPIO 5
-// GPIO 6
-// GPIO 7 - LED Laser
-// GPIO 8
-// GPIO 9
-// GPIO 10 - Motor
-// GPIO 20
-// GPIO 21
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Instant, Timer};
+use esp_backtrace as _;
+use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::timer::timg::TimerGroup;
+use log::info;
 
-fn main() -> anyhow::Result<()> {
-    esp_idf_svc::sys::link_patches();
-    let peripherals = Peripherals::take()?;
-    let mut led = PinDriver::output(peripherals.pins.gpio8)?;
-    led.set_high()?;
-    let mut timer = TimerDriver::new(peripherals.timer00, &timer::config::Config::default())?;
-    timer.enable(true)?;
-    let delay: Delay = Default::default();
+esp_bootloader_esp_idf::esp_app_desc!();
 
-    let mut adc = AdcDriver::new(peripherals.adc1, &Config::new().calibration(true))?;
-    let mut photosensor: esp_idf_hal::adc::AdcChannelDriver<0, _> =
-        AdcChannelDriver::new(peripherals.pins.gpio3)?;
+// GPIO 3  - ADC photosensor
+// GPIO 7  - LED laser
+// GPIO 8  - status LED
+// GPIO 10 - motor
+const PHOTOSENSOR_SAMPLES: u32 = 250;
+const PHOTOSENSOR_THRESHOLD: u32 = 1646;
+const MEAL_SPACING: Duration = Duration::from_secs(60 * 60 * 4);
+const DAY: Duration = Duration::from_secs(24 * 60 * 60);
 
-    let mut led = PinDriver::output(peripherals.pins.gpio7)?;
-    led.set_low()?;
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) {
+    esp_println::logger::init_logger_from_env();
 
-    let mut motor = PinDriver::output(peripherals.pins.gpio10)?;
+    let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0, peripherals.FROM_CPU_INTR0);
 
-    loop {
-        //one loop is exactly 24 hours
-        let counter_now = timer.counter().unwrap();
-        let counter_after_24_hours = counter_now + 1000000 * 60 * 60 * 24;
-        delay.delay_ms(2000);
-        rotate(&mut motor, &mut adc, &mut photosensor, &mut led);
-        delay.delay_ms(1000 * 60 * 60 * 4);
-        rotate(&mut motor, &mut adc, &mut photosensor, &mut led);
-        delay.delay_ms(1000 * 60 * 60 * 4);
-        rotate(&mut motor, &mut adc, &mut photosensor, &mut led);
+    let _status_led = Output::new(peripherals.GPIO8, Level::High, OutputConfig::default());
+    let mut laser = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
+    let mut motor = Output::new(peripherals.GPIO10, Level::Low, OutputConfig::default());
 
-        let ms_till_tomorrow: u32 = ((counter_after_24_hours - timer.counter().unwrap()) / 1000)
-            .try_into()
-            .unwrap();
+    let mut adc_config = AdcConfig::new();
+    let mut photosensor = adc_config.enable_pin(peripherals.GPIO3, Attenuation::_11dB);
+    let mut adc = Adc::new(peripherals.ADC1, adc_config);
+    let delay = Delay::new();
+    info!("catbowl started");
 
-        delay.delay_ms(ms_till_tomorrow);
-    }
-}
+    let mut rotate = || {
+        info!("feeding");
+        laser.set_high();
+        motor.set_high();
+        delay.delay_millis(2000);
 
-fn rotate(
-    motor: &mut PinDriver<Gpio10, Output>,
-    adc: &mut AdcDriver<ADC1>,
-    photosensor: &mut AdcChannelDriver<0, Gpio3>,
-    led: &mut PinDriver<Gpio7, Output>,
-) {
-    led.set_high().unwrap();
-    motor.set_high().unwrap();
-    FreeRtos::delay_ms(2000);
-    //
-    loop {
-        let mut count = 0;
-        for _ in 0..250 {
-            count += adc.read_raw(photosensor).unwrap();
-
-            Ets::delay_us(10);
+        loop {
+            let mut count = 0;
+            for _ in 0..PHOTOSENSOR_SAMPLES {
+                let result = nb::block!(adc.read_oneshot(&mut photosensor)).unwrap();
+                count += u32::from(result);
+                delay.delay_micros(10);
+            }
+            count = count / PHOTOSENSOR_SAMPLES as u32;
+            esp_println::println!("{count}");
+            if count >= PHOTOSENSOR_THRESHOLD {
+                delay.delay_millis(150);
+                motor.set_low();
+                laser.set_low();
+                break;
+            }
         }
-        if count >= 5800 {
-            FreeRtos::delay_ms(150);
-            motor.set_low().unwrap();
-            led.set_low().unwrap();
-            break;
+    };
+
+    loop {
+        let day_start = Instant::now();
+
+        Timer::after(Duration::from_secs(2)).await;
+        rotate();
+
+        Timer::after(MEAL_SPACING).await;
+        rotate();
+
+        Timer::after(MEAL_SPACING).await;
+        rotate();
+
+        let elapsed = day_start.elapsed();
+        if elapsed < DAY {
+            Timer::after(DAY - elapsed).await;
         }
     }
 }
